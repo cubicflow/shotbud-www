6d6d4ef888a59fb7e733880bb70fd24c41388836
Matt Ellison <matt@cubicflow.com>
2017-12-21 18:51:49 -0800

    started nav on large-and-up

--- _assets/js/mobile-toc.js
+++ _assets/js/mobile-toc.js
@@ -1,40 +1,40 @@
-!function(){
+// !function(){
 
-    const showTocBtn = document.querySelector('#show-mobile-toc');
-    const toc = document.querySelector('.mobile-toc');
-    const tocList = toc.querySelector('.mobile-toc__list');
-    const headings = Array.from(document.querySelectorAll('h2'));
+//     const showTocBtn = document.querySelector('#show-mobile-toc');
+//     const toc = document.querySelector('.mobile-toc');
+//     const tocList = toc.querySelector('.mobile-toc__list');
+//     const headings = Array.from(document.querySelectorAll('h2'));
 
-    const listHTML = headings.map(makeListItemTemplate).join('');
-    tocList.innerHTML = listHTML;
+//     const listHTML = headings.map(makeListItemTemplate).join('');
+//     tocList.innerHTML = listHTML;
 
-    const listItems = Array.from(document.querySelectorAll('.mobile-toc__list a'));
+//     const listItems = Array.from(document.querySelectorAll('.mobile-toc__list a'));
 
-    showTocBtn.addEventListener('click', () => {
-        toc.classList.toggle('active');
-        showTocBtn.classList.toggle('active');
-    })
+//     showTocBtn.addEventListener('click', () => {
+//         toc.classList.toggle('active');
+//         showTocBtn.classList.toggle('active');
+//     })
 
-    listItems.forEach((item, i) => {
-        item.addEventListener('click', () => {
-            closeToc();
+//     listItems.forEach((item, i) => {
+//         item.addEventListener('click', () => {
+//             closeToc();
 
-            if(i > listItems.length - 1)
-                return;
+//             if(i > listItems.length - 1)
+//                 return;
 
-            window.setTimeout(() => {
-                window.scrollBy(0, -60);
-            }, 1)
-        })
-    });
+//             window.setTimeout(() => {
+//                 window.scrollBy(0, -60);
+//             }, 1)
+//         })
+//     });
 
 
-    function makeListItemTemplate(headingNode){
-        return `<li><a href="#${headingNode.getAttribute('id')}">${headingNode.textContent}</a></li>`;
-    }
+//     function makeListItemTemplate(headingNode){
+//         return `<li><a href="#${headingNode.getAttribute('id')}">${headingNode.textContent}</a></li>`;
+//     }
 
-    function closeToc(){
-        toc.classList.remove('active');
-        showTocBtn.classList.remove('active');
-    }
-}();
+//     function closeToc(){
+//         toc.classList.remove('active');
+//         showTocBtn.classList.remove('active');
+//     }
+// }();
