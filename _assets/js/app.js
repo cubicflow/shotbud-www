44c9476b7da3883f224f473ed75ca7194f5f8ad7
Matt Ellison <matt@cubicflow.com>
2018-01-05 12:18:53 -0800

    renamed active to 'appear'

--- _assets/js/app.js
+++ _assets/js/app.js
@@ -10,6 +10,6 @@ $(document).ready(function () {
     const navigation = document.querySelector(".head__fixture");
     const masthead = document.querySelector(".masthead");
 
-    navigation.classList.toggle("active");
-    masthead.classList.toggle("active");
+    navigation.classList.toggle("appear");
+    masthead.classList.toggle("appear");
 });
