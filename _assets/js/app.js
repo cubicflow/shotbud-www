60ae2943103f58ecf1f6e88be7c0ba1a3c281d97
Matt Ellison <matt@cubicflow.com>
2018-01-05 11:26:51 -0800

    jQuery for Make Appear function on .masthead

---
+++ _assets/js/app.js
@@ -0,0 +1,13 @@
+// --------------------------------------------------
+// APP.JS
+// --------------------------------------------------
+
+//
+// Make Appear
+// --------------------------------------------------
+
+$(document).ready(function () {
+    const masthead = document.querySelector(".masthead");
+
+    masthead.classList.toggle("active");
+});
