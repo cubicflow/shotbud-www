a89834bb1e54a86809347ead5ee54f33b49deccf
Matt Ellison <matt@cubicflow.com>
2018-01-05 11:44:10 -0800

    Make Appear for head__fixture

--- _assets/js/app.js
+++ _assets/js/app.js
@@ -7,7 +7,9 @@
 // --------------------------------------------------
 
 $(document).ready(function () {
+    const navigation = document.querySelector(".head__fixture");
     const masthead = document.querySelector(".masthead");
 
+    navigation.classList.toggle("active");
     masthead.classList.toggle("active");
 });
