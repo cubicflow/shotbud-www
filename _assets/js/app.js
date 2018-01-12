fc9d6b4d06938a2a6bfb0ad90293b2f0cc2ffb4b
CubicflowDan <dan@cubicflow.com>
2018-01-12 22:11:44 +0900

    Add js to toggle FAQ

--- _assets/js/app.js
+++ _assets/js/app.js
@@ -12,4 +12,15 @@ $(document).ready(function () {
 
     navigation.classList.toggle("appear");
     masthead.classList.toggle("appear");
+
+
+    // FAQ
+    const $faqs = $('.faq__item');
+
+    $faqs.on('click', function(){
+        $(this).children('.faq__item__answer').slideToggle();
+        $(this).toggleClass('active');
+    });
+
+
 });
